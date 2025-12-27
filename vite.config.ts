159a09884832ab6f647f43fda0c5b43d83180969
rrrhhnnnn <rehankandia75@gmail.com>
2025-12-27 09:54:47 +0530

    V8

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      'bf64544e9c3e.ngrok-free.app'
+    ]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
